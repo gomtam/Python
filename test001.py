cd8de31fdb5ad6d71d27080c945f4542b0314606
Kim Taemin <kimtaemin0203@gmail.com>
2025-04-07 09:55:15 +0900

    readme 내용 수정 및 업데이트

---
+++ test001.py
@@ -0,0 +1,2 @@
+print("Hello World")
+print(456*555)
---
+++ test002.py
@@ -0,0 +1,7 @@
+print("Notepad Editer")
+print(456*555)
+apple = 16515
+name = "사과"
+a = "오늘 {0}의 가격은 1개의 {1}원 입니다.".format(name, apple)
+print(a)
+print("{}는 맛있어".format(name))
